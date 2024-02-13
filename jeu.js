8c675c70198bbb009993a69425fc573a604d5b52
Mélissa <melissa.soares1909@gmail.com>
2024-02-13 18:53:56 +0100

    fonction touche espace pour démarrer le jeu

--- jeu.js
+++ jeu.js
@@ -2,13 +2,9 @@ const personnage = document.querySelector(".personnage");
 const obstacle = document.querySelector(".obstacle1");
 const scoreDisplay = document.getElementById("valeurScore");
 
-let positionActuelle = 670;
-const positionFinale = 0;
-let score = 0;
-const vitesse = 5; // Vitesse de déplacement (en pixels par frame)
-
 function saut() {
   personnage.classList.add("saut");
+
   // Enlèvement de la fonction à la classe pour pouvoir sauter de nouveau
   setTimeout(function () {
     personnage.classList.remove("saut");
@@ -21,7 +17,25 @@ document.addEventListener("keyup", function (event) {
   }
 });
 
+let jeuDemarre = false;
+
+document.addEventListener("keydown", function (event) {
+  if (!jeuDemarre && event.code === "Space") {
+    jeuDemarre = true;
+    glisserGauche();
+  }
+});
+
+let positionActuelle = 670;
+const positionFinale = 0;
+let score = 0;
+
+// Vitesse de déplacement (en pixels par frame)
+const vitesse = 6;
+
 function glisserGauche() {
+  if (!jeuDemarre) return;
+
   if (positionActuelle > positionFinale) {
     positionActuelle -= vitesse;
     obstacle.style.left = positionActuelle + "px";
@@ -31,7 +45,6 @@ function glisserGauche() {
       score++;
       scoreDisplay.textContent = score;
     }
-
     requestAnimationFrame(glisserGauche);
   } else {
     positionActuelle = 670;
@@ -60,4 +73,14 @@ function recommencerJeu() {
   positionActuelle = 670;
   score = 0;
   scoreDisplay.textContent = score;
+}
+
+function openForm() {
+  var texte = document.querySelector(".regle");
+  texte.style.display = "block";
+}
+
+function closeForm() {
+  var texte = document.querySelector(".regle");
+  texte.style.display = "none";
 }
